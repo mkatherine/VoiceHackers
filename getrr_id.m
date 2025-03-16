1841ef35c3bcbb548ce9fa759392488be9c53dad
shannonperna <shannonperna32@gmail.com>
2025-03-15 23:12:12 -0700

    Add files via upload
    updated one of the print statements of main (not important), got getrr_id working for test 10a (import)

--- getrr_id.m
+++ getrr_id.m
@@ -1,7 +1,15 @@
 function [recognition_rate] = getrr_id(id_test,mfcc_test_data,id_train,codebook)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+% Algorithm for training the and testing the speaker recognition on the datasets.
+    % Inputs:
+    % - id_test
+    % - mfcc_test_data
+    % - id_train
+    % - codebook
+    % Output:
+    % - recognition_rate
+    % Usage: Clustering L training vectors into M codebook vectors
 
+speech = ' ';
 correct = 0;
 for i = 1:length(id_test) %test data
     test_mfcc = mfcc_test_data{i}; % MFCCs of current test speaker
@@ -16,45 +24,36 @@ for i = 1:length(id_test) %test data
         % Compute average minimum distance to codebook
         dist = sum(min(pdist2(test_mfcc', train_codebook, 'euclidean'), [], 2)) / size(test_mfcc, 1);
 
-        if dist < min_dist
+        if dist < min_dist 
             min_dist = dist;
             predicted_speaker = train_id;
         end
     end
 
     
-
-    % For test 10a (identifying type of speech):
-    if predicted_speaker > 100
-        % revert test id for correct calculations
-        if test_id < 100 
-          test_id = test_id + 100;
-        end
-
-        % add a col for idetifying 12 or 0
-        predicted_speech = floor(id_train / 100); %rounds down to nearest integer (all ids affilied with zero are <100)
-        if predicted_speech == 0
-            speech = 0;
+   % Execute only if test 10a is happening 
+   if length(codebook) > 30 
+        if (predicted_speaker > 100) && (test_id > 100) % if the predicted and test ids are coorelated to 12 
+            speech = '12';
+        elseif ((predicted_speaker < 100) && (test_id < 100)) % if the predicted and test ids are coorelated to 0
+            speech = '0';
         else
-            speech = 12;
+            speech = ''; % Else leave blank
         end
+   end
 
-    else
-       speech = ''; % Blank when not decoding speech type
-    end
+
+   
+    
    
 
 
      % Check if prediction is correct
+     
             if predicted_speaker == test_id
                 correct = correct + 1;
             end
-
-     fprintf('%10d %10d %10d %10s\n', test_id, predicted_speaker, correct, speech);
-    
-
-   
-
+fprintf('%10d %10d %10d %10s\n', test_id, predicted_speaker, correct, speech);
 end
 
 recognition_rate = (correct / length(id_test)) * 100;
